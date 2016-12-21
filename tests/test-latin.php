bc22764529852fef55e3e699c7a6e66333d9a6cb
milan <milan@srpski.biz>
2016-12-21 20:24:25 +0000

    Add many Latin tests.

--- tests/test-latin.php
+++ tests/test-latin.php
@@ -69,4 +69,58 @@ class Tests_ST_Latin extends PHPUnit_Framework_TestCase {
 		$keyword6 = new Serbian_Transliteration( 'А шта да радим?' );
 		$this->assertEquals( $keyword6->latin(), 'A šta da radim?' );
 	}
+
+	/**
+	 * Test Cyrillic inputs with mixed scripts.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function test_cyrillic_mixed_scripts_inputs() {
+		$keyword1 = new Serbian_Transliteration( 'biliЈАР' );
+		$this->assertEquals( $keyword1->latin(), 'biliJAR' );
+
+		$keyword2 = new Serbian_Transliteration( 'КuГlА' );
+		$this->assertEquals( $keyword2->latin(), 'KUGla' );
+
+		$keyword3 = new Serbian_Transliteration( 'НОСiti' );
+		$this->assertEquals( $keyword3->latin(), 'NOSiti' );
+	}
+
+	/**
+	 * Test Cyrillic inputs with non-Serbian characters.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function test_cyrillic_non_serbian_inputs() {
+		$keyword1 = new Serbian_Transliteration( 'QWERTY' );
+		$this->assertEquals( $keyword1->latin(), 'QWERTY' );
+
+		$keyword2 = new Serbian_Transliteration( 'Досије икс (X Files)' );
+		$this->assertEquals( $keyword2->latin(), 'Dosije iks (X Files)' );
+
+		$keyword3 = new Serbian_Transliteration( 'Здесь вы можете скачать свежие версии WordPress, почерпнуть полезную информацию из Кодекса или получить помощь на форуме поддержки. Подпишитесь на ленту проекта, чтобы быть в курсе событий.' );
+		$this->assertEquals( $keyword3->latin(), 'Zdesь vы možete skačatь svežie versii WordPress, počerpnutь poleznuю informaciю iz Kodeksa ili polučitь pomoщь na forume podderžki. Podpišitesь na lentu proekta, čtobы bыtь v kurse sobыtiй.' );
+	}
+
+	/**
+	 * Test inputs with Cyrillic sentences.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function test_cyrillic_sentences_inputs() {
+		$keyword1 = new Serbian_Transliteration( 'Насупрот веровању, Lorem Ipsum није насумично изабран и сложен текст.' );
+		$this->assertEquals( $keyword1->latin(), 'Nasuprot verovanju, Lorem Ipsum nije nasumično izabran i složen tekst.' );
+
+		$keyword2 = new Serbian_Transliteration( 'Његови корени потичу у делу класичне Латинске књижевности од 45. године пре нове ере, што га чини старим преко 2000 година.' );
+		$this->assertEquals( $keyword2->latin(), 'Njegovi koreni potiču u delu klasične Latinske književnosti od 45. godine pre nove ere, što ga čini starim preko 2000 godina.' );
+
+		$keyword3 = new Serbian_Transliteration( 'Richard McClintock, професор латинског на Hampden-Sydney колеџу у Вирџинији, је потражио дефиницију помало чудне речи "consectetur" из Lorem Ipsum пасуса и анализирајући делове речи у класичној књижевности отркио аутентичан извор.' );
+		$this->assertEquals( $keyword3->latin(), 'Richard McClintock, profesor latinskog na Hampden-Sydney koledžu u Virdžiniji, je potražio definiciju pomalo čudne reči "consectetur" iz Lorem Ipsum pasusa i analizirajući delove reči u klasičnoj književnosti otrkio autentičan izvor.' );
+
+		$keyword4 = new Serbian_Transliteration( 'Lorem Ipsum долази из поглавља 1.10.32 и 1.10.33 књиге "de Finibus Bonorum et Malorum" (Екстреми Бога и Зла) коју је написао Cicerо 45. године пре нове ере.' );
+		$this->assertEquals( $keyword4->latin(), 'Lorem Ipsum dolazi iz poglavlja 1.10.32 i 1.10.33 knjige "de Finibus Bonorum et Malorum" (Ekstremi Boga i Zla) koju je napisao Cicero 45. godine pre nove ere.' );
+	}
 }
